a45c2dfca9aa4da15a5db6264135c5711a3b1e7a
AlexValle97 <alexvalleoro@gmail.com>
2018-11-20 17:52:48 -0300

    Commit inicial

---
+++ src/PacientesBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PacientesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('PacientesBundle:Default:index.html.twig');
+    }
+}
